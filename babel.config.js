802fdd6d754b52e60dcb45f4258074ba1bcb2cbf
Ogooluwa Olutimilehin <85028314+ogooluwanick@users.noreply.github.com>
2022-10-11 21:23:30 +0100

    added location.js to project fixed some styling in slug.js

---
+++ babel.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+        presets: ["next/babel"],
+        plugins: [
+          ["@locator/babel-jsx/dist", {
+            env: "development",
+          }]
+        ]
+      };
+      
