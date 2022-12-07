9b3a5eb1571e8d89ea522d3bd6825923b50cd63a
Ogooluwa Olutimilehin <85028314+ogooluwanick@users.noreply.github.com>
2022-12-07 19:24:41 +0100

    creadted product backend

--- pages/api/seeder.js
+++ pages/api/seeder.js
@@ -4,8 +4,13 @@ import db from '../../lib/db';
 
 const handler = async (req, res) => {
   await db.connect();
+
   await User.deleteMany();
   await User.insertMany(data.users);
+
+  await Product.deleteMany();
+  await Product.insertMany(data.products);
+
   await db.disconnect();
   res.send({ message: 'seeded successfully' });
 };
