6af659573374090cb6bd58eeb3e2a7f4e8cab37d
Ogooluwa Olutimilehin <85028314+ogooluwanick@users.noreply.github.com>
2022-12-13 00:49:54 +0100

    fix place order btn

--- pages/api/orders/index.js
+++ pages/api/orders/index.js
@@ -5,7 +5,7 @@ import db from '../../../lib/db';
 const handler = async (req, res) => {
   const session = await getSession({ req });
   if (!session) {
-    return res.status(401).send('signin required');
+     return res.status(401).send('signin required');
   }
 
   const { user } = session;
